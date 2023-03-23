2e7d43bd341f4202b2daac1bbf5dd144efad097c
Yankeesti <timheinsberg@web.de>
2023-03-23 21:20:51 +0100

    Aufgabe eins

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,63 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Stack stack = new Stack(readValue("geben Sie die größe des Arrays an"));
+        int selected;
+        loop:
+        while(true){
+            selected = readValue("Wählen Sie eine Der Möglichkeiten:\n1) Push\n2) Pop\n3) Print\n4) Beenden",1,4);
+            switch (selected){
+                case 1:
+                    System.out.println(stack.push(readValue("geben Sie den zu Pushenden Wert ein: ")));
+                    break;
+                case 2:
+                    System.out.println(stack.pop());
+                    break;
+                case 3:
+                    stack.print();
+                    break;
+                case 4:
+                    break loop;
+            }
+        }
+    }
+
+    /**
+     * reads a Value from the console
+     * @return
+     */
+    private static int readValue(String text){
+        int output = 0;
+
+        try {
+            System.out.println(text);
+
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(System.in)
+            );
+            String input = in.readLine();
+            output = Integer.valueOf(input);
+        }
+        catch(IOException e) {
+        }
+        catch(NumberFormatException e) {
+            System.out.println("Da ist wohl etwas schiefgelaufen\nprobieren Sie es erneut");
+            System.out.println();
+            output = readValue(text);
+        }
+        return output;
+    }
+    private static int readValue(String text, int min, int max){
+        int value = readValue(text);
+        if(value >= min && value <= max){
+            return value;
+        }else{
+            System.err.println("geben Sie einen Wert zwischen " +min+" und "+ max+" ein");
+            return readValue(text,min,max);
+        }
     }
 }
---
+++ src/Stack.java
@@ -0,0 +1,48 @@
+public class Stack {
+    protected int[] stack;
+    protected int top;
+
+    public Stack(int stackSize) {
+        stack = new int[stackSize];
+        top = -1;
+    }
+
+    /**
+     * Pushs the value p on the Top of the Stack
+     *
+     * @param p
+     * @return 0 when push was successful -1 when failed
+     */
+    public int push(int p) {
+        if (top + 1 >= stack.length) {//the is no left space in the Array
+            return -1;
+        } else {
+            top++;
+            stack[top] = p;
+            return 0;
+        }
+    }
+
+    /**
+     * removes the top Value of stack
+     *
+     * @return removed value or -1 if there is no value in the stack
+     */
+    public int pop() {
+        if (top >= 0) {
+            top--;
+            return stack[top + 1];
+        } else {
+            return -1;
+        }
+    }
+
+    /**
+     * Prints the stack from the value in the top to the last
+     */
+    public void print() {
+        for (int i = top; i >= 0; i--) {
+            System.out.println(stack[i]);
+        }
+    }
+}
